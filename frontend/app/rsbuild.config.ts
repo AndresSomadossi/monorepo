67036a5e05a6fee52cb57f270c72d53659d3bdf3
Andres Somadossi <andressomadossi@gmail.com>
2025-02-02 02:30:24 -0300

    workflow

--- frontend/app/rsbuild.config.ts
+++ frontend/app/rsbuild.config.ts
@@ -1,8 +1,14 @@
-import { resolve } from 'node:path';
+// biome-ignore lint/style/useNodejsImportProtocol: <explanation>
+const path = require('path');
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-	resolve: { alias: { '@': resolve(__dirname, './src') } },
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+		extensions: ['.js', '.jsx', '.ts', '.tsx'], // Add extensions as needed
+	},
 	plugins: [pluginReact()],
 });
